ba0e2821aa23a137b3e3502badf537b54adfe5d8
bharath2232 <33877113+bharath2232@users.noreply.github.com>
2019-04-22 00:52:05 +0200

    Fixed
    Finished

---
+++ routes.js
@@ -0,0 +1,78 @@
+const Twitter = require('twitter');
+
+module.exports = (app, io) => {
+    let twitter = new Twitter({
+        consumer_key: '9MzzQgdcQWmRckmseqkXZg4uZ',
+        consumer_secret: '2ABcRhCAFHxmjTOKeUrtlcmnKdM3YQmPKyGwqjxHE3X1lcKenK',
+        access_token_key: '153031481-v6v7hL4sgBjQ9GzwhwTp253AsJ145FwNdhs4fv79',
+        access_token_secret: 'mAgSPSp1xKd1ed5F5o5LXVPRnb7HTuHyHOMkJZ1lUUrBY'
+    });
+
+    let socketConnection;
+    let twitterStream;
+
+    app.locals.searchTerm = 'JavaScript'; //Default search term for twitter stream.
+    app.locals.showRetweets = false; //Default
+
+    /**
+     * Resumes twitter stream.
+     */
+    const stream = () => {
+        twitter.stream('statuses/filter', { track: 'RCBvCSK' }, (stream) => {
+            stream.on('data', (tweet) => {
+                console.log('tiwttwr',tweet);
+                sendMessage(tweet);
+            });
+
+            stream.on('error', (error) => {
+                console.log(error);
+            });
+
+            twitterStream = stream;
+        });
+    }
+
+    /**
+     * Sets search term for twitter stream.
+     */
+    app.get('/setSearchTerm', (req, res) => {
+        let term = req.body.term;
+        app.locals.searchTerm = term;
+        stream();
+    });
+
+    /**
+     * Pauses the twitter stream.
+     */
+    app.post('/pause', (req, res) => {
+        console.log('Pause');
+        twitterStream.destroy();
+    });
+
+    /**
+     * Resumes the twitter stream.
+     */
+    app.post('/resume', (req, res) => {
+        console.log('Resume');
+        stream();
+    });
+
+    //Establishes socket connection.
+    io.on("connection", socket => {
+        socketConnection = socket;
+        stream();
+        socket.on("connection", () => console.log("Client connected"));
+        socket.on("disconnect", () => console.log("Client disconnected"));
+    });
+
+    /**
+     * Emits data from stream.
+     * @param {String} msg
+     */
+    const sendMessage = (msg) => {
+        if (msg.text.includes('RT')) {
+            return;
+        }
+        io.emit("tweets", msg);
+    }
+};
