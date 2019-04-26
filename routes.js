b82096fd36759230c5f4e7205fb46ebc794bbfdb
bharath2232 <33877113+bharath2232@users.noreply.github.com>
2019-04-27 01:01:09 +0200

    uuu

--- routes.js
+++ routes.js
@@ -2,8 +2,6 @@ const Twitter = require('twitter');
 const MongoClient = require('mongodb').MongoClient
 
 
-module.exports = (app, io) => {
-    
 const uri = "mongodb+srv://bharat:Ravi.9700@pushtoken-dgxqj.azure.mongodb.net/test?retryWrites=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 let db
@@ -13,6 +11,7 @@ client.connect(err => {
 
 });
 
+module.exports = (app, io) => {
     let twitter = new Twitter({
         consumer_key: '9MzzQgdcQWmRckmseqkXZg4uZ',
         consumer_secret: '2ABcRhCAFHxmjTOKeUrtlcmnKdM3YQmPKyGwqjxHE3X1lcKenK',
@@ -58,7 +57,7 @@ client.connect(err => {
             if (err) return console.log(err)
 
             console.log('saved to database')
-        
+            res.redirect('/')
         })
     })
     /**
